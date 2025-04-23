f6f0569d61ec8feb03120fc15bad28100cbfa89b
kavitha0802 <kavithaaudhayakumar2005gmail.com>
2025-04-23 19:02:23 +0530

    Added Js

---
+++ index.js
@@ -0,0 +1,10 @@
+//selecting side Navbar,Menu
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0
+})
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
+})
